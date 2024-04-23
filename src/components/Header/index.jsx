85904e35f18bf89cc2f1ea20e49231185f9ad107
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-04-23 11:01:58 -0300

    updates

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,32 @@
+import { NavLink } from "react-router-dom";
+import { styled } from "styled-components";
+
+const HeaderContainer = styled.header`
+    
+`;
+
 const Header = () => {
     return (
-        <>
-            <h1>header</h1>
-        </>
+        <HeaderContainer>
+            <nav>
+                <h1>LOGO</h1>
+                <ul>
+                    <li>
+                        <NavLink to={'/'}>Inicio</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={'/cardapio'}>Cardápio</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={'/promocoes'}>Promoções</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <div className="acoes">
+                <NavLink to={'entrar'}>Entrar</NavLink>
+                <button>Carrinho</button>
+            </div>
+        </HeaderContainer>
     );
 }
  
