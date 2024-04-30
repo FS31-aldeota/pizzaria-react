03c07a674e8f4b9b0da4cf12a97e4a7335196067
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-04-30 10:48:00 -0300

    updates

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,11 @@
 const Footer = () => {
     return (
-        <>
+        <a>
             <h1>Footer</h1>
             <a href="tel:+85987974616">celular</a>
-            <a href="https://api.whatsapp.com/send?phone=5585987974616&text=Vi%20seu%20perfil%20e%20quero%20te%20dar%20dinheiro">zapzap</a>
             <a href="mailto:gleidson@gmail.com">email</a>
+            <a href="https://maps.app.goo.gl/GjspYHPjh5RtYTk37">endereço do iguatemi</a>
+            <a href="https://api.whatsapp.com/send?phone=5585987974616&text=Vi%20seu%20perfil%20e%20quero%20te%20dar%20dinheiro">zapzap</a>
             <iframe 
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.248248737383!2d-38.493851011144976!3d-3.756047399999979!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c74963201865ff%3A0x2d77cda38272811f!2sShopping%20Iguatemi%20Bosque!5e0!3m2!1spt-BR!2sbr!4v1714483191064!5m2!1spt-BR!2sbr"
             width="100%"
